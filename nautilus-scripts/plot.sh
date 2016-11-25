94c19d14061d7ef24718ed08f5a5e1257476cff0
KOYAMA Ryo <koyama@jsk.imi.i.u-tokyo.ac.jp>
2016-11-25 16:35:54 +0900

    make repository python catkin package and add setup.py

--- nautilus-scripts/plot.sh
+++ nautilus-scripts/plot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 script_dir=$(cd $(dirname $(readlink -f $0 || echo $0));pwd -P)
-gnome-terminal -t "plot" -x $SHELL -ic $script_dir'/../datalogger-plotter-with-pyqtgraph.py -f ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS%.*} --plot $(zenity --file-selection --filename="'$script_dir'/../config/default.yaml" --file-filter=*.yaml) --layout $(zenity --file-selection --filename="'$script_dir'/../config/default.yaml" --file-filter=*.yaml)|| (echo -e "Fail to execute\nYou have 10s to check the error message." && sleep 10s)'
+gnome-terminal -t "plot" -x $SHELL -ic 'datalogger_plotter_with_pyqtgraph.py -f ${NAUTILUS_SCRIPT_SELECTED_FILE_PATHS%.*} --plot $(zenity --file-selection --filename="'$script_dir'/../config/default.yaml" --file-filter=*.yaml) --layout $(zenity --file-selection --filename="'$script_dir'/../config/default.yaml" --file-filter=*.yaml)|| (echo -e "Fail to execute\nYou have 10s to check the error message." && sleep 10s)'
