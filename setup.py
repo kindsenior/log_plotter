967d8bb2a99c9e4656ffa867907873304f5760e7
Ryo KOYAMA <koyama@jsk.imi.i.u-tokyo.ac.jp>
2017-02-12 11:25:19 +0900

    [setup.py] use catkin_pkg to parse package.xml instead of xml

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 #!/usr/bin/env python
-from xml.etree import ElementTree
-import os
+import os.path
+from distutils.core import setup
+from subprocess import check_output
+from catkin_pkg.python_setup import generate_distutils_setup
 
-# read package.xml and get version
-VERSION=ElementTree.parse("package.xml").getroot().find("version").text
+# read package.xml
 SCRIPTDIR = os.path.abspath(os.path.dirname(__file__))
+d=generate_distutils_setup(
+    package_xml_path=SCRIPTDIR,
+    packages=['log_plotter'],
+    package_dir={'': 'src'},
+    scripts=['src/log_plotter/datalogger_plotter_with_pyqtgraph.py'],
+    install_requires=[
+        'catkin_pkg',
+        'pyqtgraph',
+        'metayaml'
+    ],
+)
 
 # get commit hash
-from subprocess import check_output
 try:
     GIT_REVISION = check_output(["git", "rev-parse", "HEAD"]).replace("\n","")
 except:
@@ -16,17 +27,8 @@ except:
 # write into version.py
 with open(os.path.join(SCRIPTDIR, "src","log_plotter","version.py"),"w") as version_py:
     version_py.writelines("# THIS FILE IS GENERATED FROM LOG_PLOTTER SETUP.PY\n")
-    version_py.writelines("version='{}'\n".format(VERSION))
+    version_py.writelines("version='{}'\n".format(d["version"]))
     version_py.writelines("git_revision='{}'\n".format(GIT_REVISION))
-   
-# setup log_plotter using distutils
-from distutils.core import setup
-from setuptools import setup, find_packages
-from os.path import join
-setup(name='log_plotter',
-      version=VERSION,
-      description='log plotter for hrpsys',
-      packages=['log_plotter'],
-      package_dir={'': 'src'},
-      scripts=['src/log_plotter/datalogger_plotter_with_pyqtgraph.py'],
-)
+
+# setup package
+setup(**d)
